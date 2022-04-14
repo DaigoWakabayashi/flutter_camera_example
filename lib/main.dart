ff9f452a4e4e1fedc982fd83eb3b96a18ac1ec0a
DaigoWakabayashi <mamushi.journey@gmail.com>
2022-04-14 15:06:05 +0900

    create DisplayPictureScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
@@ -32,6 +34,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/// 写真撮影画面
 class TakePictureScreen extends StatefulWidget {
   const TakePictureScreen({
     Key? key,
@@ -89,11 +92,32 @@ class TakePictureScreenState extends State<TakePictureScreen> {
         onPressed: () async {
           // 写真を撮る
           final image = await _controller.takePicture();
-          // path を出力
-          print(image.path);
+          // 表示用の画面に遷移
+          await Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => DisplayPictureScreen(imagePath: image.path),
+              fullscreenDialog: true,
+            ),
+          );
         },
         child: const Icon(Icons.camera_alt),
       ),
+    );
+  }
+}
+
+// 撮影した写真を表示する画面
+class DisplayPictureScreen extends StatelessWidget {
+  const DisplayPictureScreen({Key? key, required this.imagePath})
+      : super(key: key);
+
+  final String imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('撮れた写真')),
+      body: Center(child: Image.file(File(imagePath))),
     );
   }
 }
