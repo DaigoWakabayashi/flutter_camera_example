bb3fe5810fd3b1ad0eddaf58ce03dfe534343969
DaigoWakabayashi <mamushi.journey@gmail.com>
2022-04-14 13:00:19 +0900

    update : get a list of the available cameras

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  // main 関数内で非同期処理を呼び出すための設定
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // デバイスで使用可能なカメラのリストを取得
+  final cameras = await availableCameras();
+
+  // 利用可能なカメラのリストから特定のカメラを取得
+  final firstCamera = cameras.first;
+
+  print(firstCamera);
   runApp(const MyApp());
 }
 
